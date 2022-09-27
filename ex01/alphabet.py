89dc7cb47cc6ae97e6bd15676e5632ed8dd28028
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-09-27 17:34:34 +0900

    alphabet is del

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,53 +1,50 @@
+
 import random
 import datetime
 
-num_of_alphabet = 2
-num_of_all_chars = 10
-num_of_abs_chars = 2
-num_of_trials = 2
+all_alphabet = 26
+line_alphabet = 10
+del_alphabet = 2
+challenge = 2
 
-def shutudai(alphabet):
-    all_chars = random.sample(alphabet, num_of_all_chars)
-    print("対象文字：", end="")
-    for c in sorted(all_chars): 
-        print(c, end=" ")
+def out_q(alphabet):
+    all_chr = random.sample(alphabet, line_alphabet)
+    for c in sorted(all_chr):
+        print("対象：",end=" ")
     print()
 
-    abs_chars = random.sample(all_chars, num_of_abs_chars)
-    print("表示文字：", end="")
-    for c in all_chars: 
-        if c not in abs_chars:
-            print(c, end=" ")
+    delet = random.sample(all_chr,line_alphabet)
+    print("表示文字列:",end="")
+    for i in all_chr:
+        if i not in delet:
+            print(i,end=" ")
     print()
-    return abs_chars
+    return delet
 
-
-def kaito(seikai):
-    num = int(input("欠損文字はいくつあるでしょうか？："))
-    if num != num_of_abs_chars:
-        print("不正解です．")
+def ans(anser):
+    num = int(input("無くなった文字はいくつ？＝＞"))
+    if num != del_alphabet:
+        print("ちゃうよ")
     else:
-        print("正解です．では，具体的に欠損文字を1つずつ入力してください．")
+        print("正解！んじゃぁ…何が消えとる？1個ずつ教えてくれや。")
         for i in range(num):
-            c = input(f"{i+1}文字目を入力してください：")
-            if c not in seikai:
-                print("不正解です．またチャレンジしてください．")
+            a = input(f"{i+1}文字目教えてくれや！＝＞")
+            if c not in anser:
+                print("んぁ～ちゃうな！ドンマイ！")
                 return False
             else:
-                seikai.remove(c) # 正解した場合，その文字を正解リストから削除する（重複回答を防ぐため）
+                anser.remove(c)
         else:
-            print("欠損文字も含めて完全正解です！！！")
+            print("お！すごいやん兄ちゃん！完ペキやで！")
             return True
-    return False
+        return False
 
-
-if __name__ == "__main__":
-    alphabet = [chr(i+65) for i in range(num_of_alphabet)]
-    #print(alphabet)
-    for _ in range(num_of_trials):
-        abs_chars = shutudai(alphabet)  
-        ret = kaito(abs_chars)
-        if ret:
-            break
-        else:
-            print("-"*20)
+    if __name__ == "__main__":
+        alphabet = [chr(i+65) for i in range(all_alphabet)]
+        for _ in range(challenge):
+            Delete = out_q(alphabet)
+            ret = ans(Delete)
+            if ret:
+                break
+            else:
+                print("*"*20)
