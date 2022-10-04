48005e928437b4fa2312bda046278de8f06bf87d
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-10-04 14:37:58 +0900

    calc.py 問題2やりました

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,4 +1,10 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+
+def click_number(event):
+    btn = event.widget
+    num = int(btn["text"])
+    tkm.showinfo(f"{num}", f"{num}がおされました")
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -7,6 +13,7 @@ r,c = 0, 0
 for i, num in enumerate(range(9,-1, -1), 1):
     btn = tk.Button(root, text = f"{num}", font = ("",30), width = 4, height = 2)
     btn.grid(row = r, column = c)
+    btn.bind("<1>", click_number)
     c += 1
     if i%3 == 0:
         r += 1
