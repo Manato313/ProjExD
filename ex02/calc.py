5caa6e3bc4abca23ac86357a48092c5d34b789ce
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-10-04 14:23:42 +0900

    calc.py 問題1やりました

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,28 +1,6 @@
-print("hello world")
-
 import tkinter as tk
-import tkinter.messagebox as tkm
 
-def button_click(event):
-    btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo(txt,f"[{txt}]ボタンが押されました。ｶﾞｯ‼")
 root = tk.Tk()
-root.title("ぬるぽ")
-root.geometry("500x200")
-
-label = tk.Label(root,
-                text = "これらべるだよ",
-                font=("Ricty Diminished",20)
-                )
-label.pack()
-
-button = tk.Button(root, text="NullPointerException")
-button.bind("<1>",button_click)
-button.pack()
-
-entry = tk.Entry(width = 30)
-entry.insert(tk.END,"don't")
-entry.pack()
+root.geometry("300x500")
 
 root.mainloop()
