e0bb5e7c504b43188a444608374808b83a909b91
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-10-18 14:16:18 +0900

    演習１やった

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,4 +4,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")#演習1
 
+    canv = tk.Canvas(width = 1500,height = 900, bg = "black")
+    canv.pack()
+
     root.mainloop()
