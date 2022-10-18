cd012dcdc87f03daa04cff112d73bf98b426d038
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-10-18 14:24:04 +0900

    演習4やった

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,7 +4,13 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")#演習1
 
-    canv = tk.Canvas(width = 1500,height = 900, bg = "black")
+    canv = tk.Canvas(width = 1500,height = 900, bg = "black")#演習2
     canv.pack()
+
+    tori = tk.PhotoImage(file = "fig/7.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image = tori, tag = "tori")#演習3
+
+    key = ""#演習4
 
     root.mainloop()
