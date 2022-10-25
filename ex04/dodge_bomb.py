2cdfb88c51f1ffbb1b1aa3b5ccc65abb3319e7ab
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-10-25 15:28:42 +0900

    練習問題５やった

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,25 +1,32 @@
 import pygame as pg
 import sys
+from random import randint
 
 def main():
-    # 練習1
+
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
-    # 練習3
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
+    bomb_sfc = pg.Surface((20, 20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = randint(0, scrn_rct.width)
+    bomb_rct.centery = randint(0, scrn_rct.height)
 
-    clock = pg.time.Clock() # 練習1
+    clock = pg.time.Clock()
     while True:
-        scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
+        scrn_sfc.blit(bg_sfc, bg_rct)
         
-        for event in pg.event.get(): # 練習2
+        for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
@@ -28,17 +35,18 @@ def main():
         if key_states[pg.K_DOWN]:  tori_rct.centery += 1
         if key_states[pg.K_LEFT]:  tori_rct.centerx -= 1
         if key_states[pg.K_RIGHT]: tori_rct.centerx += 1
- 
 
-        scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
 
-        pg.display.update() #練習2
+        scrn_sfc.blit(tori_sfc, tori_rct)
+
+        pg.display.update()
         clock.tick(1000)
 
 
 
 if __name__ == "__main__":
-    pg.init() # 初期化
-    main() # ゲームの本体
-    pg.quit() # 初期化の解除
+    pg.init() 
+    main() 
+    pg.quit() 
     sys.exit()
