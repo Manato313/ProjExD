a5672c7c0a6683e2478dd91df42f361613183db0
Manato313 <c0b211673c@edu.teu.ac.jp>
2022-11-08 16:59:37 +0900

    rdy_fight実装(仮置きでこうかとんです)

--- ex06/main.py
+++ ex06/main.py
@@ -24,8 +24,8 @@ class rdy_fight():
         self.rct = self.sfc.get_rect()
         self.rct.center = xy
 
-    def blit(self):
-        self.sfc.blit(self.sfc, self.rct)
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
 
 
 #class Mode:
@@ -53,22 +53,18 @@ class rdy_fight():
 def main():
 
     scr = Screen("刹那test", (1200, 800), "fig/pg_bg.jpg")
-    #fight = rdy_fight("fig/6.png", 2.0, (600, 300))
+    fight = rdy_fight("fig/6.png", 2.0, (600, 300))
     diley_frame = randint(2500,5000)# ms 2.5秒～5.0秒
     clock = pg.time.Clock()
     flag = 0
     while True:
         scr.blit() # 背景表示
-        #print (pg.time.get_ticks()) #デバッグ用
-        #if pg.time.get_ticks() >= diley_frame:
-        #    fight.blit()
-        #    flag = 1 #フラグ1にする
-        #    img1 = pg.image.load("fig/5.png")
-            #Screen.blit(img1)
+        print (pg.time.get_ticks()) #デバッグ用
+        if pg.time.get_ticks() >= diley_frame:
+            fight.blit(scr)
+            flag = 1 #フラグ1にする
         key_states = pg.key.get_pressed()
-        #print(key_states[pg.K_SPACE])
         if key_states[pg.K_SPACE]:
-            return
             if flag == 1:
                 return
             if flag == 0:
